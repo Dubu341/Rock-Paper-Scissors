8480e2096865b3ccae90fa7ffe39b13eba1b9882
Dubu341 <aersvhtukl@proton.me>
2026-09-18 12:10:17 +0100

    add game round logic

--- index.js
+++ index.js
@@ -3,9 +3,38 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-console.log(getComputerChoice());
+
 
 function getHumanChoice()  {
-   let sign = prompt();
+    let sign = prompt("Rock, Paper, or Scissors:");
+    let input = sign.trim().toLowerCase();
+    return input;
 }
-console.log(getHumanChoice());
+
+const humanScore = 0;
+const computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == computerChoice){
+        console.log("TIE!")
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You Win!");
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You Win!");
+    }
+    else if (humanChoice ===  "scissors" && computerChoice === "paper"){
+        console.log("You Win!");
+    }
+    else {
+        console.log ("You Lose!")
+    }
+}
+
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    console.log("You:", humanChoice)
+    console.log("Computer:", computerChoice);
+    playRound(humanChoice, computerChoice);
